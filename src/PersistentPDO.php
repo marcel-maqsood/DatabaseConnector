99d0b22b890d6083db78f805bb6994b2b5aa651f
REIKEM\mmaqsood <mmaqsood@reikem.de>
2025-08-27 17:33:17 +0200

    Added: - fetchSQL function which takes SQL and first arg, params(array) as non required second arg for params that has been used within the sql, debug as third arg for displaying the sql before execution.

--- src/PersistentPDO.php
+++ src/PersistentPDO.php
@@ -7,157 +7,194 @@ namespace MazeDEV\DatabaseConnector;
 
 use PDO;
 
-class PersistentPDO 
+class PersistentPDO
 {
 
 	public $sqlList = [];
 
-    private PDO $pdo;
+	private PDO $pdo;
 
 	private $showSqlLog = false;
 
-    public function __construct(PDO $pdo, bool $showSqlLog)
-    {
-        $this->pdo = $pdo;
+	public function __construct(PDO $pdo, bool $showSqlLog)
+	{
+		$this->pdo = $pdo;
 		$this->showSqlLog = $showSqlLog;
 	}
-    
-    public function getPDO() : PDO 
-    {
-        return $this->pdo;
-    }
 
-    /**
-     * Fetches data from the database based on specified conditions.
-     *
-     * This function constructs and executes a SQL query to retrieve data from the database.
-     *
-     * @param string $field - The field in which to search for the $identifier.
-     * @param string $table - The table in which to search.
-     * @param array|string $conditions - An array oe string of conditions to filter the results.
-     * @param array $joins - An array that defines which tables should be used for certain columns.
-     * @param array $groupDetails - An array that defines how multiple row values can be grouped.
-     * 
-     * conditions are getting appended after "WHERE" don't include it.
-     * 
-     * The first condition should not include a 'logicalOperator' key, as there is no preceding condition to combine with, also it won't get used.
-     *   Example:
-     *   $conditions = [
-     *      'name' => [
-     *          'logicalOperator' => 'AND' // The logical operator to combine conditions: 'AND' or 'OR'
-     *          'operator' => 'LIKE',  // The operation that this condition must match.
-     *          'queue' => 'admin',    // The value to look for in the 'name' field.
-     *          'wildcard' => 'both'   // The mode for character matching: before | after | both | none .
-     *      ]
-     *   ];
-     * $conditions could be a @param string aswell, example: "name = 'test'"
-     * @return string|null - The result of the query.
-     */
-    public function get(string $field, $table, array|string $conditions = [], array $joins = [], $groupDetails = [], $debug = false) : string|object|null
-    {        
-        //Generate sql and check if any condition is given
-        $sql;
-        $joins = $this->generateSQLJoins($joins);
-        $finalConditionString = $this->generateConditionString($table, $conditions);
-        $groups = "";
+	public function getPDO() : PDO
+	{
+		return $this->pdo;
+	}
 
-        if(!empty($groupDetails))
-        {
-            $groups = ",". $this->generateGroupingString($groupDetails['groups']);
-        }
+	/**
+	 * Fetches data from the database based on specified conditions.
+	 *
+	 * This function constructs and executes a SQL query to retrieve data from the database.
+	 *
+	 * @param string $field - The field in which to search for the $identifier.
+	 * @param string $table - The table in which to search.
+	 * @param array|string $conditions - An array oe string of conditions to filter the results.
+	 * @param array $joins - An array that defines which tables should be used for certain columns.
+	 * @param array $groupDetails - An array that defines how multiple row values can be grouped.
+	 *
+	 * conditions are getting appended after "WHERE" don't include it.
+	 *
+	 * The first condition should not include a 'logicalOperator' key, as there is no preceding condition to combine with, also it won't get used.
+	 *   Example:
+	 *   $conditions = [
+	 *      'name' => [
+	 *          'logicalOperator' => 'AND' // The logical operator to combine conditions: 'AND' or 'OR'
+	 *          'operator' => 'LIKE',  // The operation that this condition must match.
+	 *          'queue' => 'admin',    // The value to look for in the 'name' field.
+	 *          'wildcard' => 'both'   // The mode for character matching: before | after | both | none .
+	 *      ]
+	 *   ];
+	 * $conditions could be a @param string aswell, example: "name = 'test'"
+	 * @return string|null - The result of the query.
+	 */
+	public function get(string $field, $table, array|string $conditions = [], array $joins = [], $groupDetails = [], $debug = false) : string|object|null
+	{
+		//Generate sql and check if any condition is given
+		$sql;
+		$joins = $this->generateSQLJoins($joins);
+		$finalConditionString = $this->generateConditionString($table, $conditions);
+		$groups = "";
+
+		if(!empty($groupDetails))
+		{
+			$groups = ",". $this->generateGroupingString($groupDetails['groups']);
+		}
 
 
-        $sql = "SELECT $table.*" . $groups . " FROM " . $table . $joins . $finalConditionString . ($groups != "" ? ' GROUP BY ' . $groupDetails['identifier'] . ' ' : ' ') . ';';
+		$sql = "SELECT $table.*" . $groups . " FROM " . $table . $joins . $finalConditionString . ($groups != "" ? ' GROUP BY ' . $groupDetails['identifier'] . ' ' : ' ') . ';';
 
 		if($this->showSqlLog === true){
 			$this->sqlList[] = $sql;
 		}
 
-        if($debug)
-        {
-            var_dump($sql);
-            exit;
-        }
+		if($debug)
+		{
+			var_dump($sql);
+			exit;
+		}
 
-        $stmt = $this->pdo->prepare($sql);
-        if (false === $stmt) 
-        {
-            throw new \Exception\RuntimeException('PDO error: ' . $e->getMessage());
-        }
+		$stmt = $this->pdo->prepare($sql);
+		if (false === $stmt)
+		{
+			throw new \Exception\RuntimeException('PDO error: ' . $e->getMessage());
+		}
 
-        $stmt->execute();
-        $result = $stmt->fetchObject();
+		$stmt->execute();
+		$result = $stmt->fetchObject();
 
-        if (! $result) 
-        {
-            return null;
-        }
+		if (! $result)
+		{
+			return null;
+		}
 
-        if($field == '*')
-        {
-            return $result;
-        }
+		if($field == '*')
+		{
+			return $result;
+		}
 
-        return strval($result->{$field});
-    }
+		return strval($result->{$field});
+	}
 
-    /**
-     * For further details, see $this->get(); as this method uses the same syntax.
-     * This function will always return an array with all fields that got found with given conditions.
-     * 
-     * @return array|null - The result array of the query.
-     */
-    public function getAll($table, array|string $conditions = [], array $joins = [], $groupDetails = [], $orderBy = "", array $cols = [], $debug = false)
-    {
-        $sql;
-        $joins = $this->generateSQLJoins($joins);
-        $finalConditionString = $this->generateConditionString($table, $conditions);
-        $groups = "";
-        $orderString = "";
-        if($orderBy != "")
-        {
-            $orderString = 'ORDER BY ' . $orderBy;
-        }
-
-
-        if(!empty($groupDetails))
-        {
-            $groups = ",". $this->generateGroupingString($groupDetails['groups']);
-        }
+	/**
+	 * For further details, see $this->get(); as this method uses the same syntax.
+	 * This function will always return an array with all fields that got found with given conditions.
+	 *
+	 * @return array|null - The result array of the query.
+	 */
+	public function getAll($table, array|string $conditions = [], array $joins = [], $groupDetails = [], $orderBy = "", array $cols = [], $debug = false)
+	{
+		$sql;
+		$joins = $this->generateSQLJoins($joins);
+		$finalConditionString = $this->generateConditionString($table, $conditions);
+		$groups = "";
+		$orderString = "";
+		if($orderBy != "")
+		{
+			$orderString = 'ORDER BY ' . $orderBy;
+		}
 
 
-        $select = "";
-
-        if(!empty($cols))
-        {
-            foreach($cols as $col)
-            {
-                $select .= "$col, ";
-            }
-        }
-        else
-        {
-            $select = "$table.*" ;
-        }
-
-        $select = rtrim($select, ', ');
+		if(!empty($groupDetails))
+		{
+			$groups = ",". $this->generateGroupingString($groupDetails['groups']);
+		}
 
 
+		$select = "";
 
-        $sql = "SELECT " . $select . $groups . " FROM " . $table . $joins . $finalConditionString . ($groups != "" ? ' GROUP BY ' . $groupDetails['identifier'] . ' ' : ' ') . $orderString . ';';
-        return $this->getAllBase($sql, $debug);
-    }
+		if(!empty($cols))
+		{
+			foreach($cols as $col)
+			{
+				$select .= "$col, ";
+			}
+		}
+		else
+		{
+			$select = "$table.*" ;
+		}
+
+		$select = rtrim($select, ', ');
 
 
-     /**
-     * getAllBase() expects a sql string as first argument and prepares, executes and returns just that.
-     * This function will always return an array with all fields that got found with given conditions or NULL.
-     * 
-     * @param string $sql - The SQL Statement that should be executed
-     * @param bool $debug - Wether or not the functiion should dump the current sql for debugging.
-     * 
-     * @return array|null - The result array of the query.
-     */
+
+		$sql = "SELECT " . $select . $groups . " FROM " . $table . $joins . $finalConditionString . ($groups != "" ? ' GROUP BY ' . $groupDetails['identifier'] . ' ' : ' ') . $orderString . ';';
+		return $this->getAllBase($sql, $debug);
+	}
+
+
+	/**
+	 * fetchSql() expects a sql string as first argument and prepares, executes and returns just that.
+	 * This function will always return an array with all fields that got found with given conditions or NULL.
+	 *
+	 * @param string $sql - The SQL Statement that should be executed
+	 * @param array $params - params that might be used within the sql
+	 * @param bool $debug - Wether or not the functiion should dump the current sql for debugging.
+	 *
+	 * @return array|null - The result array of the query.
+	 */
+	public function fetchSql($sql, $params = [], $debug = false)
+	{
+		if ($debug) {
+			var_dump($sql, $params);
+			exit;
+		}
+
+		if ($this->showSqlLog === true) {
+			$this->sqlList[] = $sql;
+		}
+
+		$stmt = $this->pdo->prepare($sql);
+		if ($stmt === false) {
+			throw new \RuntimeException("PDO error: SQL Statement couldn't be prepared.");
+		}
+
+		$stmt->execute($params);
+		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		if (empty($results)) {
+			return null;
+		}
+
+		return $results;
+	}
+
+	/**
+	 * getAllBase() expects a sql string as first argument and prepares, executes it and returns it as edited array.
+	 * This function will always return an array with all fields that got found with given conditions or NULL.
+	 *
+	 * @param string $sql - The SQL Statement that should be executed
+	 * @param bool $debug - Wether or not the functiion should dump the current sql for debugging.
+	 * @param array $params - params that might be used within the sql
+	 *
+	 * @return array|null - The result array of the query.
+	 */
 	public function getAllBase($sql, $debug = false, $params = [])
 	{
 		if ($debug) {
@@ -211,144 +248,144 @@ class PersistentPDO
 
 
 	/**
-     * Updates data in the database based on specific conditions
-     * 
-     * @param array $updates - An array with fields and the new values.
-     * For further details, see $this->get(); as this method uses the same syntax for conditions.
-     */
-    public function update($table, array $updates, array|string $conditions = [], bool $debug = false) : bool
-    {
-        $sql = "UPDATE " . $table . " SET " . $this->generateUpdateSQLString($updates) . (($conditions === [] || $conditions === null) ? '' :  $this->generateConditionString($table, $conditions));
+	 * Updates data in the database based on specific conditions
+	 *
+	 * @param array $updates - An array with fields and the new values.
+	 * For further details, see $this->get(); as this method uses the same syntax for conditions.
+	 */
+	public function update($table, array $updates, array|string $conditions = [], bool $debug = false) : bool
+	{
+		$sql = "UPDATE " . $table . " SET " . $this->generateUpdateSQLString($updates) . (($conditions === [] || $conditions === null) ? '' :  $this->generateConditionString($table, $conditions));
 
 		if($this->showSqlLog === true){
 			$this->sqlList[] = $sql;
 		}
 
-        if($debug)
-        {
-            var_dump($sql);
-            exit;
-        }
+		if($debug)
+		{
+			var_dump($sql);
+			exit;
+		}
 
-        $stmt = $this->pdo->prepare($sql);
+		$stmt = $this->pdo->prepare($sql);
 
-        if ($stmt === null) 
-        {
-            throw new \Exception\RuntimeException('PDO error: ' . $e->getMessage());
-        }
+		if ($stmt === null)
+		{
+			throw new \Exception\RuntimeException('PDO error: ' . $e->getMessage());
+		}
 
 
 
-        return $stmt->execute();
-    }
+		return $stmt->execute();
+	}
 
-    /**
-     * Inserts data in the database based on specific conditions
-     * 
-     * @param string $table - The table in which this entry should be added
-     * @param array $inserts - An array with fields and values.
-     * @return id of inserted row or false, if our statement ran into an issue.
-     * Example:
-     * $inserts = [
-     * 
-     *   'field' => 'value'
-     * 
-     * ]
-     */
-    public function insert($table, array $inserts, $debug = false) : int|bool
-    {
-        try 
-        {
-            $this->pdo->beginTransaction();
+	/**
+	 * Inserts data in the database based on specific conditions
+	 *
+	 * @param string $table - The table in which this entry should be added
+	 * @param array $inserts - An array with fields and values.
+	 * @return id of inserted row or false, if our statement ran into an issue.
+	 * Example:
+	 * $inserts = [
+	 *
+	 *   'field' => 'value'
+	 *
+	 * ]
+	 */
+	public function insert($table, array $inserts, $debug = false) : int|bool
+	{
+		try
+		{
+			$this->pdo->beginTransaction();
 
-            $sql = "INSERT INTO " . $table . " " . $this->generateInsertString($inserts);
+			$sql = "INSERT INTO " . $table . " " . $this->generateInsertString($inserts);
 
 			if($this->showSqlLog === true){
 				$this->sqlList[] = $sql;
 			}
 
-            if ($debug) 
-            {
-                var_dump($sql);
-                exit;
-            }
+			if ($debug)
+			{
+				var_dump($sql);
+				exit;
+			}
 
-            $stmt = $this->pdo->prepare($sql);
+			$stmt = $this->pdo->prepare($sql);
 
-            if (false === $stmt) 
-            {
-                throw new \Exception\RuntimeException('PDO error: ' . $this->pdo->errorInfo());
-            }
+			if (false === $stmt)
+			{
+				throw new \Exception\RuntimeException('PDO error: ' . $this->pdo->errorInfo());
+			}
 
-            if ($stmt->execute()) 
-            {
-                $lastInsertId = (int) $this->pdo->lastInsertId();
-                $this->pdo->commit();
-                return $lastInsertId;
-            }
+			if ($stmt->execute())
+			{
+				$lastInsertId = (int) $this->pdo->lastInsertId();
+				$this->pdo->commit();
+				return $lastInsertId;
+			}
 
-            $this->pdo->rollBack();
-            return false;
-        } 
-        catch (\Exception $e) 
-        {
-            $this->pdo->rollBack();
-            throw $e;
-        }
-    }
+			$this->pdo->rollBack();
+			return false;
+		}
+		catch (\Exception $e)
+		{
+			$this->pdo->rollBack();
+			throw $e;
+		}
+	}
 
-    /**
-     * Deletes data in the database based on specific conditions
-     * For further details about conditions, see get(); as this method uses syntax.
-     */
-    public function delete($table, array|string $conditions, $debug = false) : bool
-    {
-        $sql;
-        $finalConditionString = $this->generateConditionString($table, $conditions);
+	/**
+	 * Deletes data in the database based on specific conditions
+	 * For further details about conditions, see get(); as this method uses syntax.
+	 */
+	public function delete($table, array|string $conditions, $debug = false) : bool
+	{
+		$sql;
+		$finalConditionString = $this->generateConditionString($table, $conditions);
 
-        $sql = "DELETE FROM " . $table . $finalConditionString;
+		$sql = "DELETE FROM " . $table . $finalConditionString;
 
 		if($this->showSqlLog === true){
 			$this->sqlList[] = $sql;
 		}
 
-        if($debug)
-        {
-            var_dump($sql);
-            exit;
-        }
+		if($debug)
+		{
+			var_dump($sql);
+			exit;
+		}
 
-        $stmt = $this->pdo->prepare($sql);
-        if (false === $stmt) 
-        {
-            throw new \Exception\RuntimeException('PDO error: ' . $e->getMessage());
-        }
- 
-         return $stmt->execute();
-    }
+		$stmt = $this->pdo->prepare($sql);
+		if (false === $stmt)
+		{
+			throw new \Exception\RuntimeException('PDO error: ' . $e->getMessage());
+		}
+
+		return $stmt->execute();
+	}
 
 
-    #
-    #
-    #
-    # Functions below this comment may be moved into a seperate utility class.
-    #
-    #
-    #
-    private function generateGroupingString($groups)
-    {
-        $groupedString = "";
-        foreach($groups as $group)
-        {
-            if($groupedString === "")
-            {
-                $groupedString .= " GROUP_CONCAT(DISTINCT ".$group['for'].") AS " . $group['as'];
-                continue;
-            }
-            $groupedString .= ", GROUP_CONCAT(DISTINCT ".$group['for'].") AS " . $group['as'];
-        }
-        return $groupedString;
-    }
+	#
+	#
+	#
+	# Functions below this comment may be moved into a seperate utility class.
+	#
+	#
+	#
+	private function generateGroupingString($groups)
+	{
+		$groupedString = "";
+		foreach($groups as $group)
+		{
+			if($groupedString === "")
+			{
+				$groupedString .= " GROUP_CONCAT(DISTINCT ".$group['for'].") AS " . $group['as'];
+				continue;
+			}
+			$groupedString .= ", GROUP_CONCAT(DISTINCT ".$group['for'].") AS " . $group['as'];
+		}
+		return $groupedString;
+	}
 
 	private function generateInsertString(array $inserts) : string
 	{
@@ -382,7 +419,7 @@ class PersistentPDO
 			if ($value === null) {
 				$escapedValue = "NULL";
 			} else {
-				$escapedValue = $this->pdo->quote($value);
+				$escapedValue = $this->pdo->quote($value . "");
 			}
 
 			// SQL-Teilstück zusammenbauen
@@ -398,11 +435,11 @@ class PersistentPDO
 
 
 	/**
-     * 
-     * Appends all conditions, handles each of them as configured
-     * 
-     * @return string
-     */
+	 *
+	 * Appends all conditions, handles each of them as configured
+	 *
+	 * @return string
+	 */
 	public function generateConditionString($table, $conditions): string
 	{
 		$bondConditions = "";
@@ -484,19 +521,19 @@ class PersistentPDO
 		}
 	}
 
-    private function generateSQLJoins(array $joins = [])
-    {
-        $joinString = "";
+	private function generateSQLJoins(array $joins = [])
+	{
+		$joinString = "";
 
-        foreach ($joins as $join) 
-        {
-            if (isset($join['table']) && isset($join['on'])) 
-            {
-                $joinString .= " LEFT JOIN " . $join['table'] . " ON " . $join['on'];
-            }
-        }
+		foreach ($joins as $join)
+		{
+			if (isset($join['table']) && isset($join['on']))
+			{
+				$joinString .= " LEFT JOIN " . $join['table'] . " ON " . $join['on'];
+			}
+		}
 
-        return $joinString;
-    }
+		return $joinString;
+	}
 
 }
